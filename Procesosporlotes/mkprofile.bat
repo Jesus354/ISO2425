85e94f5d181c84e826b1c5c39fefb1937dfde765
Jesus354 <98556813+Jesus354@users.noreply.github.com>
2024-11-18 09:51:47 +0100

    Add files via upload

---
+++ Procesosporlotes/mkprofile.bat
@@ -0,0 +1,10 @@
+@echo off
+Rem script mkprofile.bat
+Rem autor: Jesús Iglesias Muñoz
+Rem Fecha: 13-11-2024
+mkdir %userprofile%\asir %userprofile%\dam %userprofile%\smr %userprofile%\daw %userprofile%\ceti
+mkdir %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\exam
+mkdir %userprofile%\dam\docs %userprofile%\dam\pract %userprofile%\dam\exam
+mkdir %userprofile%\smr\docs %userprofile%\smr\pract %userprofile%\smr\exam
+mkdir %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\exam
+mkdir %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\exam
